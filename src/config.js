ef24ba5100cd44c55055f437b405ba13daade405
Noah <aminnnoah19@gmail.com>
2021-01-11 14:47:48 -0600

    vercel ready

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://still-brushlands-38243.herokuapp.com/',
+  // API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
