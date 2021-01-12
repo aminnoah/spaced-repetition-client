6b914037c208f908ae10212ce9a3be9ea3350439
Noah <aminnnoah19@gmail.com>
2021-01-12 10:42:23 -0600

    changed api endpoint

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
-export default {
-
-  
-  API_ENDPOINT: 'https://still-brushlands-38243.herokuapp.com',
+export default {  
+  API_ENDPOINT: 'https://still-brushlands-38243.herokuapp.com/api',
   // API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
