8684c4a7df7ab8e82ac246130311c223d099ea89
Noah <aminnnoah19@gmail.com>
2021-01-13 12:52:38 -0600

    added secure endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {  
-  API_ENDPOINT: 'http://spaced-repetition-creole.herokuapp.com/api',
+  API_ENDPOINT: 'https://spaced-repetition-creole.herokuapp.com/api',
    //API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
